a638fa12b407aa69261f9ad8c0b79ae0804f1be9
Nathan <hui.n1998@gmail.com>
2022-06-28 19:17:12 +0100

    feat: setup graphql api

---
+++ pages/api/graphql.ts
@@ -0,0 +1,38 @@
+import { ApolloServer, gql } from "apollo-server-micro";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const typeDefs = gql`
+  type Query {
+    getTest: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    getTest: () => {
+      console.log("resolver called");
+      return "hello";
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+const startServer = server.start();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await startServer;
+  await server.createHandler({ path: "/api/graphql" })(req, res);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
