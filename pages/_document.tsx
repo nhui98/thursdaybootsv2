5ab607e7e63c342f8801ad676fce2e976916f5ac
Nathan <hui.n1998@gmail.com>
2022-07-03 13:36:13 +0100

    feat: implement custom doc adding logo font

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitalProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initalProps = await Document.getInitialProps(ctx);
+    return { ...initalProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
