8c953117b6ace7b13aa5fa37f27487e5634c50fb
Nathan <hui.n1998@gmail.com>
2022-07-07 18:28:44 +0100

    feat: feature component data and images

---
+++ components/Featured/data.ts
@@ -0,0 +1,43 @@
+export interface featuredType {
+  img: string;
+  title: string;
+  href: string;
+}
+
+export const featuredData: featuredType[] = [
+  {
+    img: "/images/featured/featured-1.webp",
+    title: "men's boots",
+    href: "/",
+  },
+  {
+    img: "/images/featured/featured-2.webp",
+    title: "women's boots",
+    href: "/",
+  },
+  {
+    img: "/images/featured/featured-3.webp",
+    title: "men's shoes",
+    href: "/",
+  },
+  {
+    img: "/images/featured/featured-4.webp",
+    title: "men's loafers",
+    href: "/",
+  },
+  {
+    img: "/images/featured/featured-5.webp",
+    title: "men's sneakers",
+    href: "/",
+  },
+  {
+    img: "/images/featured/featured-6.webp",
+    title: "women's sneakers",
+    href: "/",
+  },
+  {
+    img: "/images/featured/featured-7.webp",
+    title: "women's heels",
+    href: "/",
+  },
+];
