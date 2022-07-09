ab3e981583de55775ac056d4ea4b48907c25a2b5
Nathan <hui.n1998@gmail.com>
2022-07-10 00:51:16 +0100

    fix: vercel deploy with mongoose 3

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -6,6 +6,13 @@ const conn = {
 
 const dbConnect = async () => {
   if (conn.isConnected) return;
+
+  console.log(process.env.MONGO_URI);
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("where is mongo uri?");
+  }
+
   const db = await mongoose.connect(process.env.MONGO_URI as string);
   // @ts-ignore
   conn.isConnected = db.connections[0].readyState;
