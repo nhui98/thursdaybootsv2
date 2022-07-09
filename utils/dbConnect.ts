887219210af7ff81c46eb253284e845ef50dc2ce
Nathan <hui.n1998@gmail.com>
2022-07-10 00:39:12 +0100

    fix: vercel deploy with mongoose

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,7 +1,15 @@
 import mongoose from "mongoose";
 
+const conn = {
+  isConnected: false,
+};
+
 const dbConnect = async () => {
-  await mongoose.connect(process.env.MONGO_URI as string);
+  if (conn.isConnected) return;
+  const db = await mongoose.connect(process.env.MONGO_URI as string);
+  // @ts-ignore
+  conn.isConnected = db.connections[0].readyState;
+  // await mongoose.connect(process.env.MONGO_URI as string);
 };
 
 export default dbConnect;
