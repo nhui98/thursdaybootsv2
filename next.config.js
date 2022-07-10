1051fe05c8c0118744eda5a8d5a88678a19001b7
Nathan <hui.n1998@gmail.com>
2022-07-10 10:47:49 +0100

    fix: vecel mongoose deployment 11

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://khao:kaotik23@cluster0.l2iu8.mongodb.net/thursdayboots?retryWrites=true&w=majority",
-  },
 };
 
 module.exports = nextConfig;
