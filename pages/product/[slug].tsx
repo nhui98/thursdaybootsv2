3713689eee074c5e8d3893b63f5c8b946405fdab
Nathan <hui.n1998@gmail.com>
2022-07-14 21:32:12 +0100

    feat: product details page

---
+++ pages/product/[slug].tsx
@@ -0,0 +1,77 @@
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import ProductDescription from "../../components/ProductDescription/ProductDescription";
+import ProductDetail from "../../components/ProductDetail/ProductDetail";
+import ProductImages from "../../components/ProductImages/ProductImages";
+import s from "../../styles/pages/productDetails.module.scss";
+import dbConnect from "../../utils/dbConnect";
+import Product, { ProductType } from "../../utils/models/Product";
+
+const ProductDetails: NextPage = ({ product }: any) => {
+  const {
+    images,
+    slug,
+    style,
+    price,
+    color,
+    sizes,
+    productFeatures,
+    featureImage,
+    description,
+  } = JSON.parse(product) as ProductType;
+
+  return (
+    <div className={s.productDetails}>
+      <section className={s.container}>
+        <ProductImages images={images} slug={slug} />
+        <ProductDetail
+          style={style}
+          price={price}
+          color={color}
+          sizes={sizes}
+          productFeatures={productFeatures}
+        />
+      </section>
+
+      <ProductDescription
+        style={style}
+        featureImage={featureImage}
+        description={description}
+        slug={slug}
+      />
+
+      {/* <section className={s.reviews}>reviews</section> */}
+    </div>
+  );
+};
+
+export default ProductDetails;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  await dbConnect();
+  const products = await Product.find().select("slug");
+  const paths = products.map((p) => {
+    return {
+      params: { slug: p.slug },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  await dbConnect();
+
+  if (!params?.slug || Array.isArray(params?.slug))
+    throw new Error("bad parameters");
+
+  const product = await Product.findOne({ slug: params.slug });
+
+  return {
+    props: {
+      product: JSON.stringify(product),
+    },
+  };
+};
