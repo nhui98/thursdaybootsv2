95ae46c74e5759ce5b02170021a0f2e252f97d0e
Nathan <hui.n1998@gmail.com>
2022-07-19 17:32:08 +0100

    feat: user resolvers

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,17 +1,19 @@
 import { ApolloServer } from "apollo-server-micro";
 import { NextApiRequest, NextApiResponse } from "next";
 import Product from "../../utils/models/Product";
+import User from "../../utils/models/User";
 import { resolvers } from "../../utils/resolvers";
 import { typeDefs } from "../../utils/schema";
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req, res }) => {
+  context: async ({ req, res }) => {
     return {
       req,
       res,
       Product,
+      User,
     };
   },
 });
